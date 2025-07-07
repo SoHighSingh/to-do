c397094bfc610dae65b836e90b94b68cd5731017
Agam Singh <agamsohi@gmail.com>
2025-07-07 17:07:00 +1000

    made some front-end changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,33 +17,11 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">brevvvvv</span> App
+            <span className="text-[hsl(280,100%,70%)]">Sign in</span> and write something <span className="text-[hsl(280,100%,70%)]">Funny</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://youtube.com"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                shut up
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                no.
-              </div>
-            </Link>
-          </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              whats good 
+              click this king &#8595;
             </p>
 
             <div className="flex flex-col items-center justify-center gap-4">
@@ -54,7 +32,7 @@ export default async function Home() {
                 href={session ? "/api/auth/signout" : "/api/auth/signin"}
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
               >
-                sign innnn
+                {session ? "sign out" : "sign in"}
               </Link>
             </div>
           </div>
