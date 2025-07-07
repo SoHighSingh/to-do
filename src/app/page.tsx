7c767ec8146dbffd3be4833543c47d44855b68d9
Agam Singh <agamsohi@gmail.com>
2025-07-07 16:19:08 +1000

    Added Google Auth, added Supabase connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,18 +17,17 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Create <span className="text-[hsl(280,100%,70%)]">brevvvvv</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="https://youtube.com"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                shut up
               </div>
             </Link>
             <Link
@@ -38,14 +37,13 @@ export default async function Home() {
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                no.
               </div>
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+              whats good 
             </p>
 
             <div className="flex flex-col items-center justify-center gap-4">
@@ -56,7 +54,7 @@ export default async function Home() {
                 href={session ? "/api/auth/signout" : "/api/auth/signin"}
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
               >
-                {session ? "Sign out" : "Sign in"}
+                sign innnn
               </Link>
             </div>
           </div>
