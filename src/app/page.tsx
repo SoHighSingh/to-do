47e6fbce17e3ac13901bb603cb62e1f2c60b933e
Agam Singh <agamsohi@gmail.com>
2025-07-08 14:03:04 +1000

    More Design Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
@@ -14,23 +13,23 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#f7ebab] to-[#ffc054] text-[#383838]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">Sign in</span> and write something <span className="text-[hsl(280,100%,70%)]">Funny</span>
+            Create your <span className="text-[hsl(33,100.00%,60.20%)]">To-Do List</span> .
           </h1>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              click this king &#8595;
+            <p className="text-2xl text-[#383838]">
+              {session ? "" : "Get Started"}
             </p>
 
             <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
+              <p className="text-center text-2xl text-black">
                 {session && <span>Logged in as {session.user?.name}</span>}
               </p>
               <Link
                 href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+                className="rounded-full bg-white/50 px-10 py-3 font-semibold transition hover:bg-white/30"
               >
                 {session ? "sign out" : "sign in"}
               </Link>
